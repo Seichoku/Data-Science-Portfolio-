51aaaeb285da8ff41557e2ef30c40f922eaf99a7
Emeka Ichoku <78593818+Seichoku@users.noreply.github.com>
2023-10-26 19:19:09 +0100

    coding NLTK

---
+++ nltk_utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import nltk
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer
+
+def tokenize(sentence):
+    """
+    split sentence into an array of words or
+    token
+    """
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    """
+    steming = finding the root form of the word 
+    """
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+
+    # stem each word 
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words:
+            bad[idx] = 1
+
+    return bag
+    
